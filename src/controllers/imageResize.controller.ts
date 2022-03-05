c695654226dc454827244f7b8254cf1421adcab4
lucas andrade <lcs.oliveira93@gmail.com>
2022-03-05 13:02:24 -0300

    Feat: adding controllers files

---
+++ src/controllers/imageResize.controller.ts
@@ -0,0 +1,26 @@
+import { Request, Response, NextFunction } from "express";
+import { Container, Service } from "typedi";
+import { ImageResizeLogic } from "../logic";
+
+@Service()
+export class ImageResizeController {
+  private logic: ImageResizeLogic;
+
+  constructor() {
+    this.logic = Container.get(ImageResizeLogic);
+  }
+
+  async handle(req: Request, res: Response, next: NextFunction) {
+    const { imageName, height, width } = req.params;
+
+    const response = await this.logic.execute({
+      imageName: imageName as string,
+      height: Number(height),
+      width: Number(width),
+    });
+    return res
+      .status(200)
+      .setHeader("content-type", "text/html")
+      .send(response);
+  }
+}

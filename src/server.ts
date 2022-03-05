ed2397271803e54b1a32654d47a42e175ee0ee5f
lucas andrade <lcs.oliveira93@gmail.com>
2022-03-05 14:57:48 -0300

    Feat: adding error handler class and implementation in server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express, NextFunction, Request, Response } from "express";
 import "reflect-metadata";
+import { AppError } from "./errors/AppError";
 import { getRoutes } from "./routes";
 
 const app = express();
@@ -7,5 +8,20 @@ const app = express();
 app.use(express.json());
 
 app.use(getRoutes());
+
+app.use(
+  (error: Error, requrest: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        message: error.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: "Error",
+      message: `Interal Server Error - ${error.message}`,
+    });
+  }
+);
 
 app.listen(5050, () => console.log(`Server is running on port: 5050`));

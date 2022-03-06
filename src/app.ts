15a0cd331d96baa4d89d8aeb2b564f6b421eab3e
lucas andrade <lcs.oliveira93@gmail.com>
2022-03-06 19:47:27 -0300

    feat: separate app and server file

---
+++ src/app.ts
@@ -0,0 +1,30 @@
+import express, { Express, NextFunction, Request, Response } from "express";
+import "reflect-metadata";
+import "express-async-errors";
+import * as Sentry from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
+import { AppError } from "./errors/AppError";
+import { getRoutes } from "./routes";
+
+const app = express();
+
+app.use(express.json());
+
+app.use(getRoutes());
+
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        message: error.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: "Error",
+      message: `Interal Server Error - ${error.message}`,
+    });
+  }
+);
+
+export { app };

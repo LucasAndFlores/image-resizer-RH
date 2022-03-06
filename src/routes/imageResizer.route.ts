c3bcb9e0e6602b774b3466350cb87ba78dd241cd
lucas andrade <lcs.oliveira93@gmail.com>
2022-03-06 19:48:38 -0300

    feat: add validator in route files

--- src/routes/imageResizer.route.ts
+++ src/routes/imageResizer.route.ts
@@ -1,6 +1,8 @@
 import { Router, Request, Response, NextFunction } from "express";
 import Container from "typedi";
 import { ImageResizeController } from "../controllers";
+import { ImageResizeValidator } from "./schemas";
+import { RouteValidator } from "./validation";
 
 export const imageResizeRouter = () => {
   const router = Router();
@@ -9,6 +11,7 @@ export const imageResizeRouter = () => {
 
   router.get(
     "/resize/:imageName/:width/:height",
+    RouteValidator.validate(ImageResizeValidator.get()),
     async (req: Request, res: Response, next: NextFunction) => {
       await controller.handle(req, res, next);
     }

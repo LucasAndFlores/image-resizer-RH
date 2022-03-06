ae622a1384177eb7534e7f1b6e35b584bc4db463
lucas andrade <lcs.oliveira93@gmail.com>
2022-03-06 19:16:17 -0300

    feat: server file refac

--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,6 @@
-import express, { Express, NextFunction, Request, Response } from "express";
-import "reflect-metadata";
-import "express-async-errors";
-import { AppError } from "./errors/AppError";
-import { getRoutes } from "./routes";
+import { app } from "./app";
+require("dotenv").config();
 
-const app = express();
-
-app.use(express.json());
-
-app.use(getRoutes());
-
-app.use(
-  (error: Error, request: Request, response: Response, next: NextFunction) => {
-    if (error instanceof AppError) {
-      return response.status(error.statusCode).json({
-        message: error.message,
-      });
-    }
-
-    return response.status(500).json({
-      status: "Error",
-      message: `Interal Server Error - ${error.message}`,
-    });
-  }
+app.listen(process.env.PORT, () =>
+  console.log(`Server is running on port: ${process.env.PORT}`)
 );
-
-app.listen(5050, () => console.log(`Server is running on port: 5050`));
